a5f71bc115a3d314f900ee2f9cbef63ec00b246a
pudding0803 <nakohui160@gmail.com>
2023-02-15 15:41:32 +0800

    change whole framework from pygame to PyQt6

--- Color.py
+++
@@ -1,8 +0,0 @@
-class Color:
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    BACKGROUND = (170, 208, 255)
-    PIANO_W = (255, 252, 243)
-    PIANO_B = (10, 13, 24)
-    PRESS_W = (255, 56, 96)
-    PRESS_B = (50, 115, 220)
--- main.py
+++ main.py
@@ -1,150 +1,69 @@
 import sys
+import threading
+import time
 
-import pygame as pg
-from pygame import mixer
+from PyQt6 import QtWidgets, QtGui
+from PyQt6.QtCore import Qt, QUrl
+from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
 
-from Color import Color
-from Setting import Setting
+from ui import Ui_MainWindow
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.pitch = 4
+        self.w_keys = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
+        self.b_keys = ['S', 'D', 'G', 'H', 'J']
+        self.players = {key: QMediaPlayer() for key in self.w_keys + self.b_keys}
+        self.audios = {key: QAudioOutput() for key in self.w_keys + self.b_keys}
+        for player, audio in zip(self.players.values(), self.audios.values()):
+            player.setAudioOutput(audio)
+        # filename, filetype = QFileDialog.getOpenFileName(self, '選擇資料集', './',
+        #                                                  '文字文件 (*.txt)')
+        # print(filename, filetype)
+
+    def keyPressEvent(self, event):
+        match event.key():
+            case Qt.Key.Key_Z.value | Qt.Key.Key_X.value | Qt.Key.Key_C.value | Qt.Key.Key_V.value | \
+                 Qt.Key.Key_B.value | Qt.Key.Key_N.value | Qt.Key.Key_M.value | Qt.Key.Key_S.value | \
+                 Qt.Key.Key_D.value | Qt.Key.Key_G.value | Qt.Key.Key_H.value | Qt.Key.Key_J.value:
+                key = chr(event.key())
+                self.players[key].setSource(QUrl.fromLocalFile(f'assets\\audio\\piano\\{self.pitch}\\{key}.mp3'))
+                self.players[key].play()
+                threading.Thread(target=self.key_on, args=(key,)).start()
+            case Qt.Key.Key_Down.value | Qt.Key.Key_Left.value:
+                self.pitch = max(1, self.pitch - 1)
+                self.ui.pitch.setText(str(self.pitch))
+                threading.Thread(target=self.pitch_on, args=('left',)).start()
+            case Qt.Key.Key_Up.value | Qt.Key.Key_Right.value:
+                self.pitch = min(7, self.pitch + 1)
+                self.ui.pitch.setText(str(self.pitch))
+                threading.Thread(target=self.pitch_on, args=('right',)).start()
+
+    def key_on(self, key: str):
+        getattr(self.ui, f'key_{key}').setStyleSheet(
+            f'background-color: {"#FF3860" if key in self.w_keys else "#3273DC"};'
+        )
+        time.sleep(0.3)
+        getattr(self.ui, f'key_{key}').setStyleSheet(
+            f'background-color: {"#FFFCF3" if key in self.w_keys else "#0A0D18"};'
+        )
+
+    def pitch_on(self, direction: str):
+        getattr(self.ui, f'img_{direction}').setStyleSheet(f'border-image: url("assets/image/{direction}_on.png");')
+        time.sleep(0.3)
+        getattr(self.ui, f'img_{direction}').setStyleSheet(f'border-image: url("assets/image/{direction}.png");')
 
 
 def main():
-    pitch = 4
-    key_time = [[0 for _ in range(8)], [0 for _ in range(7)]]
-    pitch_time = [0, 0]
-    pg.init()
-    mixer.init()
-    pg.display.set_caption('Simple Sheet Music Maker')
-    pg.display.set_icon(pg.image.load('assets/icon/favicon.ico'))
-    font = pg.font.Font('assets/font/SpaceJaeger-BW2pn.otf', 24)
-    surface = pg.display.set_mode((Setting.WIDTH, Setting.HEIGHT))
-    surface.fill(Color.BACKGROUND)
-    tools = ['file', 'save', 'play', 'stop', 'record', 'edit']
-    t_keys = ['Q', 'W', 'E', 'R', 'T', 'Y']
-    text = font.render('pitch', True, Color.WHITE, None)
-    rect = text.get_rect()
-    rect.center = (Setting.PITCH_X, Setting.PITCH_Y)
-    surface.blit(text, rect)
-    while True:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-            elif event.type == pg.KEYDOWN:
-                match event.key:
-                    case pg.K_z:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\0\\1.mp3')
-                        mixer.music.play()
-                        key_time[0][0] = Setting.PRESS_TIME
-                    case pg.K_x:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\0\\2.mp3')
-                        mixer.music.play()
-                        key_time[0][1] = Setting.PRESS_TIME
-                    case pg.K_c:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\0\\3.mp3')
-                        mixer.music.play()
-                        key_time[0][2] = Setting.PRESS_TIME
-                    case pg.K_v:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\0\\4.mp3')
-                        mixer.music.play()
-                        key_time[0][3] = Setting.PRESS_TIME
-                    case pg.K_b:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\0\\5.mp3')
-                        mixer.music.play()
-                        key_time[0][4] = Setting.PRESS_TIME
-                    case pg.K_n:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\0\\6.mp3')
-                        mixer.music.play()
-                        key_time[0][5] = Setting.PRESS_TIME
-                    case pg.K_m:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\0\\7.mp3')
-                        mixer.music.play()
-                        key_time[0][6] = Setting.PRESS_TIME
-                    case pg.K_s:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\1\\1.mp3')
-                        mixer.music.play()
-                        key_time[1][1] = Setting.PRESS_TIME
-                    case pg.K_d:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\1\\2.mp3')
-                        mixer.music.play()
-                        key_time[1][2] = Setting.PRESS_TIME
-                    case pg.K_g:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\1\\4.mp3')
-                        mixer.music.play()
-                        key_time[1][4] = Setting.PRESS_TIME
-                    case pg.K_h:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\1\\5.mp3')
-                        mixer.music.play()
-                        key_time[1][5] = Setting.PRESS_TIME
-                    case pg.K_j:
-                        mixer.music.load(f'assets\\audio\\piano\\{pitch}\\1\\6.mp3')
-                        mixer.music.play()
-                        key_time[1][6] = Setting.PRESS_TIME
-                    case pg.K_DOWN | pg.K_LEFT:
-                        pitch = max(1, pitch - 1)
-                        pitch_time[0] = Setting.PRESS_TIME
-                    case pg.K_UP | pg.K_RIGHT:
-                        pitch = min(7, pitch + 1)
-                        pitch_time[1] = Setting.PRESS_TIME
-
-        # Render Tools
-        for i, tool in enumerate(tools):
-            pg.draw.rect(surface, Color.BACKGROUND,
-                         pg.Rect(Setting.TOOL_SPACE * i + Setting.TOOL_X - 5, Setting.TOOL_Y - 5, 60, 60))
-            img = pg.transform.scale(pg.image.load(f'assets/image/{tool}.png').convert_alpha(), (50, 50))
-            img.set_alpha(100)
-            surface.blit(img, (Setting.TOOL_SPACE * i + Setting.TOOL_X, Setting.TOOL_Y))
-            pg.draw.circle(surface, Color.WHITE,
-                           (Setting.TOOL_SPACE * i + Setting.TOOL_X + 50, Setting.TOOL_Y + 45), 16, 0)
-            text = font.render(t_keys[i], True, Color.BLACK, None)
-            rect = text.get_rect()
-            rect.center = (Setting.TOOL_SPACE * i + Setting.TOOL_X + 50, Setting.TOOL_Y + 45)
-            surface.blit(text, rect)
-
-        # Render Piano Keys
-        w_keys = ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
-        b_keys = ['A', 'S', 'D', 'F', 'G', 'H', 'J']
-        for i in range(7):
-            pg.draw.rect(surface, Color.PRESS_W if key_time[0][i] else Color.PIANO_W,
-                         pg.Rect(64 * i + Setting.MARGIN_LEFT, 260, 60, 250))
-            text = font.render(w_keys[i], True, Color.WHITE if key_time[0][i] else Color.BLACK, None)
-            rect = text.get_rect()
-            rect.center = (64 * i + Setting.MARGIN_LEFT + 30, 460)
-            surface.blit(text, rect)
-            if key_time[0][i]:
-                key_time[0][i] -= 1
-            if i == 0 or i == 3:
-                continue
-            pg.draw.rect(surface, Color.PRESS_B if key_time[1][i] else Color.PIANO_B,
-                         pg.Rect(64 * i + Setting.MARGIN_LEFT - 20, 260, 40, 130))
-            text = font.render(b_keys[i], True, Color.WHITE, None)
-            rect = text.get_rect()
-            rect.center = (64 * i + Setting.MARGIN_LEFT, 350)
-            surface.blit(text, rect)
-            if key_time[1][i]:
-                key_time[1][i] -= 1
-
-        # Render Pitch
-        pg.draw.rect(surface, Color.BACKGROUND, pg.Rect(Setting.PITCH_X + 60, Setting.PITCH_Y - 15, 30, 30))
-        text = font.render(str(pitch), True, Color.WHITE, None)
-        rect = text.get_rect()
-        rect.center = (Setting.PITCH_X + 75, Setting.PITCH_Y)
-        surface.blit(text, rect)
-        if pitch_time[0]:
-            img = pg.transform.scale(pg.image.load('assets/image/left_on.png').convert_alpha(), (40, 40))
-            pitch_time[0] -= 1
-        else:
-            img = pg.transform.scale(pg.image.load('assets/image/left.png').convert_alpha(), (40, 40))
-        surface.blit(img, (Setting.PITCH_X - 105, Setting.PITCH_Y - 20))
-        if pitch_time[1]:
-            img = pg.transform.scale(pg.image.load('assets/image/right_on.png').convert_alpha(), (40, 40))
-            pitch_time[1] -= 1
-        else:
-            img = pg.transform.scale(pg.image.load('assets/image/right.png').convert_alpha(), (40, 40))
-        surface.blit(img, (Setting.PITCH_X + 105, Setting.PITCH_Y - 20))
-
-        # Update
-        pg.display.update()
-        pg.time.Clock().tick(30)
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    QtGui.QFontDatabase.addApplicationFont('assets\\font\\SpaceJaeger-BW2pn.otf')
+    window.show()
+    sys.exit(app.exec())
 
 
 if __name__ == '__main__':
